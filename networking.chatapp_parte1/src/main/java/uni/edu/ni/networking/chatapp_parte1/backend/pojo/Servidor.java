d04c05a66ab604e1c0279f823c25c1eaedf7b440
ArielRoa35 <arielroa123@gmail.com>
2021-04-26 23:47:35 -0600

    Chat dividido en partes para cada pc.
    Primera y segunda parte del programa "mini chat".

---
+++ networking.chatapp_parte1/src/main/java/uni/edu/ni/networking/chatapp_parte1/backend/pojo/Servidor.java
@@ -0,0 +1,57 @@
+package uni.edu.ni.networking.chatapp_parte1.backend.pojo;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Servidor extends Observable implements Runnable{
+
+    private int puerto;
+
+    public Servidor(int puerto) {
+        this.puerto = puerto;
+    }
+     
+    
+    
+    @Override
+    public void run() {
+        
+        ServerSocket servidor = null;
+        Socket sc = null;
+        DataInputStream in;
+        DataOutputStream out;
+        
+        try {
+            
+            servidor = new ServerSocket(puerto);
+            System.out.println("Servidor iniciado");
+            
+            while(true){
+                
+                sc = servidor.accept();
+                System.out.println("Cliente conectado");
+                
+                in = new DataInputStream(sc.getInputStream());
+                
+                String mensaje = in.readUTF();
+                System.out.println(mensaje);
+                
+                this.setChanged();
+                this.notifyObservers(mensaje);
+                this.clearChanged();
+                
+                sc.close();
+                System.out.println("Cliente desconectado");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ networking.chatapp_parte2/src/main/java/uni/edu/ni/networking/chatapp_parte2/backend/pojo/Cliente.java
@@ -0,0 +1,40 @@
+package uni.edu.ni.networking.chatapp_parte2.backend.pojo;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Cliente implements Runnable{
+    
+    private int puerto;
+    private String mensaje;
+    private String host;
+
+    public Cliente(int puerto, String mensaje, String host) {
+        this.puerto = puerto;
+        this.mensaje = mensaje;
+        this.host = host;
+    }
+    
+    @Override
+    public void run() {
+
+        DataOutputStream out;
+        
+        try {
+            
+            Socket sc = new Socket(host, puerto);
+            
+            out = new DataOutputStream(sc.getOutputStream());
+            
+            out.writeUTF(mensaje);
+            
+            sc.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
